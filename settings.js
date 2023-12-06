a2fa8046c8506ffd7fe8ce10750dbaa1a19955f6
janilog <151744657+janilog@users.noreply.github.com>
2023-12-07 00:14:34 +0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
-global.ownername = 'Dark Alpha'
+global.owner = ['03170082529']
+global.premium = ['03170082529']
+global.ownernomer = '03170082529'
+global.ownername = 'Jutt'
 global.botname = 'BIXBY-MD USER BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
 global.ig = 'https://github.com/darkalphaxteam'
